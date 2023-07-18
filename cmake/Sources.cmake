341e3c135c582f19f16acb03478d7d3ee2b143f7
LightTab2 <azorro12@gmail.com>
2023-07-18 15:17:40 +0200

    Qt Quick (#1)
    * Cpp standard fix; tests fix
    
    Fixes linkage in tests. C++ standard is based on ${CMAKE_CXX_STANDARD} now
    
    * QtQuick, boost linked properly, no export header
    
    There is no longer automatic generation  of export header for dll export/import because it uses wrong macro. The correct one is Q_DECL_EXPORT/Q_DECL_IMPORT and the header needs to be created manually

---
+++ cmake/Sources.cmake
@@ -0,0 +1,7 @@
+file(GLOB SOURCE_FILES
+     src/*.cpp
+     src/*.h
+     src/*.ui)
+
+file(GLOB TEST_FILES 
+     test/*.cpp)
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <boost/filesystem.hpp>
 
 int main(int argc, char *argv[])
 {
+    boost::filesystem::recursive_directory_iterator it;
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
