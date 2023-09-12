c0dba4fba1d9d7b890596ce43bd90cbd828bf9df
LightTab2 <azorro12@gmail.com>
2023-09-12 20:27:27 +0200

    Adds QoL conan scripts

--- icon/WinScripts/fixUnixScripts.ps1
+++ icon/WinScripts/fixUnixScripts.ps1
@@ -1,21 +1,43 @@
-$targetDir = "..\UnixScripts"
+$targetPaths = @(
+    "..\UnixScripts",
+    "..\..\conanLibrariesInstall.sh"
+)
 
-if (-not (Test-Path -Path $targetDir -PathType Container)) {
-    Write-Host "The target directory does not exist."
-    exit 1
+foreach ($targetPath in $targetPaths) {
+    if (-not (Test-Path -Path $targetPath)) {
+        Write-Host "The target path '$targetPath' does not exist."
+        continue
+    }
+
+    if ((Get-Item $targetPath).PSIsContainer) {
+        # Process a directory
+        Get-ChildItem -Path $targetPath | ForEach-Object {
+            $file = $_.FullName
+            Write-Host "Processing file: $file"
+
+            # Read the content of the file and replace carriage return characters with empty strings
+            $content = Get-Content -Path $file -Raw
+            $content = $content -replace "\r", ""
+
+            # Write the modified content back to the same file without adding a newline at the end
+            Set-Content -Path $file -Value $content -NoNewline
+        }
+
+        Write-Host "All carriage return characters removed from files in $targetPath."
+    }
+    else {
+        # Process a single file
+        $file = $targetPath
+        Write-Host "Processing file: $file"
+
+        # Read the content of the file and replace carriage return characters with empty strings
+        $content = Get-Content -Path $file -Raw
+        $content = $content -replace "\r", ""
+
+        # Write the modified content back to the same file without adding a newline at the end
+        Set-Content -Path $file -Value $content -NoNewline
+
+        Write-Host "All carriage return characters removed from $file."
+    }
 }
-
-Get-ChildItem -Path $targetDir | ForEach-Object {
-    $file = $_.FullName
-    Write-Host "Processing file: $file"
-
-    # Read the content of the file and replace carriage return characters with empty strings
-    $content = Get-Content -Path $file -Raw
-    $content = $content -replace "\r", ""
-
-    # Write the modified content back to the same file
-    Set-Content -Path $file -Value $content -NoNewline
-}
-
-Write-Host "All carriage return characters removed from files in $targetDir."
 Pause
